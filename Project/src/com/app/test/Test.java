6ddee699bb3eac27741e25f236cd590ac18b38bc
Rajesh <Rajesh@DESKTOP-GKB890B>
2018-06-10 22:44:13 +0530

    comiit by Tl

--- Project/src/com/app/test/Test.java
+++ Project/src/com/app/test/Test.java
@@ -7,13 +7,18 @@ public class Test {
 		System.out.println(x);
 	}
 	
-	
+	public int sub(int x,int y)
+	{
+		int z=x-y;
+		return z;
+	}
 	public static void main(String[] args) {
 		
 	System.out.println("abc");
 
 	Test t=new Test();
 	t.add();
+	t.sub(10, 58);
 	
 	}
 

505322250f90dc2a2b3664cefa2a23adaacc1a34
Rajesh <Rajesh@DESKTOP-GKB890B>
2018-06-10 22:23:59 +0530

    commit by dev1

--- Project/src/com/app/test/Test.java
+++ Project/src/com/app/test/Test.java
@@ -1,8 +1,12 @@
 package com.app.test;
 
 public class Test {
-
+	public void add() {
+		int x=10+20;
+		System.out.println(x);
+	}
 	public static void main(String[] args) {
+		
 	System.out.println("abc");
 
 	}
